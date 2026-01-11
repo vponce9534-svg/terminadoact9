3cc0b2162e8dd59a192b8aaa4ccd5c875027578a
vponce9534-svg <vponce9534@utm.edu.ec>
2026-01-10 22:43:06 -0500

    Change API base URL to production endpoint

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000";
+const API_BASE = "https://terminadoact9-backend.onrender.com";
 const token = localStorage.getItem("token");
 if (!token) window.location.href = "index.html";
 
